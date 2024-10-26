239040862f5766e544fc7cf6a95cf2736079b85f
feederbox826 <feederbox826@users.noreply.github.com>
2024-10-26 00:16:05 -0400

    ipv6 listen

--- src/server.js
+++ src/server.js
@@ -93,13 +93,13 @@ update()
 // run twice daily
 schedule("0 */12 * * *", update)
 
-async function startFastify(port = 9988) {
-  console.log(`Server running on http://localhost:${port}`);
+async function startFastify(port = 9988, address = "::") {
+  console.log(`Server running on http://${address}:${port}`);
   await fastify.register(cors)
   await fastify.register(fastifyStatic, {
     root: path.join(import.meta.dirname, 'public'),
   })
-  await fastify.listen({ port, address: "0.0.0.0" })
+  await fastify.listen({ port, address })
 }
 startFastify()
 
